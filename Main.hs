8dd8eae53806c1ddf2e4a02a06f52fdf62afef73
Lucas Åström <lucasas@kth.se>
2015-08-31 01:24:27 +0200

    changed isGameOver to winnerOf, returns a Maybe Player; implemented negamax
    also added a dependency for either-unwrap

--- Main.hs
+++ Main.hs
@@ -1,7 +1,8 @@
-import Data.Vector (Vector, generate, slice, toList, (!), (//))
-import Data.Either
+import Data.Vector (Vector, generate, slice, toList, fromList, (!), (//))
+import Data.Either.Unwrap
 import Data.Maybe
-import Debug.Trace
+import Data.List
+import System.IO
 
 data Player = X | O deriving (Eq, Show, Enum)
 type Tile = Either Int Player
@@ -26,23 +27,30 @@ nextPlayer :: Player -> Player
 nextPlayer X = O
 nextPlayer O = X
 
-isGameOver :: State -> Bool
-isGameOver s = any (==True) $ map same rows ++ map same cols ++ map same diags
+negamax :: Int -> Player -> State -> Int
+negamax d p s
+        | d == 0 || isJust winner = if p == fromJust winner then 100 else -100
+        | all isRight $ board s = 0
+        | otherwise = minimum $
+            map (negate . negamax (d - 1) (nextPlayer p)) (nextStates s)
     where
-        same [a, b, c] = all isRight [a, b, c] && a == b && b == c
+        winner = winnerOf s
+
+
+winnerOf :: State -> Maybe Player
+winnerOf s = maybe Nothing id $
+        find isJust $ map winner rows ++ map winner cols ++ map winner diags
+    where
+        winner [a, b, c] = 
+            if all isRight [a, b, c] && a == b && b == c
+            then Just (fromRight a) else Nothing
         rows  = map (\i -> toList $ slice (3*i) 3 (board s)) [0..2]
         cols  = map (\i -> map ((!) $ board s) [i, i+3, i+6]) [0..2]
         diags = map (map ((!) (board s))) [[0, 4, 8], [2, 4, 6]]
 
 -- temporary tests to see if shit works
 main = do
-    print initialState
-    print $ makeMove 0 $ initialState
-    --print $ isGameOver $ makeMove 0 $ initialState
-    --let row = foldl (flip (.)) id (map makeMove [0, 3, 1, 4, 2]) $ initialState
-    --print $ row
-    --print $ isGameOver row
-    print $ take 2 . toList . board $ makeMove 6 initialState
-    print $ take 2 . toList . board $ makeMove 6 initialState
-    print $ take 5 $ nextStates initialState
-    print $ take 5 $ nextStates initialState
+    print $ map (negamax 10 X) $ nextStates
+        $ State (fromList [Right X, Right O, Left 2,
+                           Left 3, Left 4, Left 5,
+                           Left 6, Left 7, Left 8]) X
